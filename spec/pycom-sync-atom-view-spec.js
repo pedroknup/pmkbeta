9972c471d097b7cf551b1ba7169f16cc0a6a73f3
Pedro Knup <phknup@gmail.com>
2020-02-05 15:00:27 +0100

    first commit

---
+++ spec/pycom-sync-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+// import PycomSyncAtomView from '../lib/pmkbeta-view';
+//
+// describe('PycomSyncAtomView', () => {
+//   it('has one valid test', () => {
+//     expect('life').toBe('easy');
+//   });
+// });
